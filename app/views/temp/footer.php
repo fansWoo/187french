40a6f90dcdefcb1392605c8be85590a7218af402
juliafanswoo <julia@fanswoo.com>
2015-03-20 18:26:24 +0800

    index

--- app/views/temp/footer.php
+++ app/views/temp/footer.php
@@ -1,47 +1,16 @@
 		</div>
 		<div class="footer">
+			<img src="app/img/index/dessert01.png" class="dessert01">
 			<div class="centerArea">
-				<div class="leftbox">
-					<div class="box">
-						<h2>關於</h2>
-						<a href="">公司簡介</a>
-						<a href="">產品與市場應用</a>
-						<a href="">聯繫我們</a>
-						<h2>新聞與活動</h2>
-						<a href="">公司簡介</a>
-						<a href="">產品與市場應用</a>
-						<a href=""></a>
-						<a href="">聯繫我們</a>
-					</div>
-					<div class="box">
-						<h2>更多介紹</h2>
-						<a href="">技術與支援</a>
-						<a href="">其它頁面</a>
-						<a href="">支援與技術</a>
-						<a href="">其它自訂動態頁面</a>
-						<a href="">技術與支援</a>
-						<a href="">其它頁面</a>
-						<a href="">支援與技術</a>
-					</div>
-					<div class="box">
-						<a href=""><p class="Partner">News Letter</p></a>
-						<a href=""><p class="Partner">Partner Zone</p></a>
-					</div>
+				<div class="leftBox">
+					<p>週二~週六 9:00~18:00</p>
+					<p>(02)-2882-1871</p>
+					<p>台北市士林區中正路187巷2號</p>
 				</div>
-				<div class="rightbox">
-					<div class="box">
-						<img src="app/img/logo.png" class="footer_logo">
-						<p>旭浩股份有限公司</p>
-						<p>SOLNINE 9 INC</p>
-						<p>新北市新店區民權路95號12樓之3</p>
-						<p>TEL :  02-8219-3336</p>
-						<p>FAX :  02-8219-1835</p>
-					</div>
-				
+				<div class="rightBox">
+					<p>FB(https://www.facebook.com/187patissiere)</p>
+					<p>sservice@187patissiere.com.tw</p>
 				</div>
-			</div>
-			<div class="bottomBox">
-					Designed by fansWoo.com
 			</div>
 		</div>
 	</body>
